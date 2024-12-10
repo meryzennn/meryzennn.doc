d2574d08d870f1efd594b898059f2fde93f6427a
meryzennn <me.ryzennn@gmail.com>
2024-12-10 23:45:27 +0700

    display credit false

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
     src: 'assets/images/bg3.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: true,         // Display the credit text of the banner image
+      enable: false,         // Display the credit text of the banner image
       text: '',              // Credit text to be displayed
       url: 'https://www.pixiv.net/en/artworks/117902447'                // (Optional) URL link to the original artwork or artist's page
     }
